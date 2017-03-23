d387bd04bd10ab60ab388f02c36e617a7ed528c4
Sean.Henry <fidelicatessen@gmail.com>
2017-03-23 15:22:39 -0700

    Finish color game

--- UdemyHTML/RGBColorGuessingGame.js
+++ UdemyHTML/RGBColorGuessingGame.js
@@ -1,72 +1,56 @@
-// var colors = [
-// 	"rgb(255, 0, 0)",
-// 	"rgb(0, 255, 0)",
-// 	"rgb(0, 0, 255)",
-// 	"rgb(255, 255, 0)",
-// 	"rgb(255, 0, 255)",
-// 	"rgb(0, 255, 255)"			
-// ];
-// var pickedColor = colors[3];
-
-// var squares = document.querySelectorAll(".square");
-// document.getElementById("targetColor").textContent = pickedColor.toUpperCase();
-// for(var i = 0; i < squares.length; i++) {
-// 	// add initial colors to squares
-// 	squares[i].style.backgroundColor = colors[i];
-// 	// add click listeners to squares
-// 	squares[i].addEventListener("click", function(){
-// 		// grab color of picked square
-// 		var clickedColor = this.style.backgroundColor;
-// 		// compare color to pickedColor
-// 		if(clickedColor === pickedColor){
-// 			alert("Correct");
-// 		} else {
-// 			alert("Incorrect");
-// 		}
-// 	})
-// }
 var h1 = document.querySelector("h1");
 var reset = document.getElementById("reset");
 var message = document.getElementById("message");
 var easy = document.getElementById("easy");
 var hard = document.getElementById("hard");
-
+var squares = document.querySelectorAll(".square");
+var selected = document.querySelector(".selected");
 var easyMode = false;
 
 boardSetUp(easyMode);
 
-reset.addEventListener("click", boardSetUp);
-easy.addEventListener("click", function(){
-		var easyMode = true;
-		boardSetUp(easyMode);
-	});
-hard.addEventListener("click", function(){
-		var easyMode = false;
-		boardSetUp(easyMode);
-	});	
+// event listeners for clicks on reset, easy and hard buttons. Easy
+// and hard buttons change the state of easyMode and also change highlighting
+// depending on which one is currently active.
+reset.addEventListener("click", function(){
+	boardSetUp(easyMode);
+});
 
-function boardSetUp() {	
-	h1.style.backgroundColor = "#232323";
+easy.addEventListener("click", function(){
+		this.classList.add("selected");
+		hard.classList.remove("selected");
+		easyMode = true;		
+		boardSetUp(easyMode);
+});
+
+hard.addEventListener("click", function(){
+		this.classList.add("selected");
+		easy.classList.remove("selected");
+		easyMode = false;
+		boardSetUp(easyMode);
+});	
+
+// This is the main game engine which ensures that all squares in play are full
+// of randomly selected colors, picks the winning color and then listens for the user
+// to click on a color, making it disappear if it's wrong, or running the success()
+// function if they pick correctly.
+function boardSetUp(easyMode) {	
+	h1.style.backgroundColor = "steelblue";	
 	reset.textContent = "NEW COLORS";
-	// set up random color array	
+	message.textContent = "";
+	// set up random color array with varying length depending on difficulty setting	
 	if(easyMode){
-		var length = 3;
+		var length = 3;		
 	} else {
-		var length = 6;
+		var length = 6;		
 	};	
-	var colors = colorSelector(6);
+	var colors = colorSelector(length);
 	// randomly select one color from array to be picked color and put the RGB value
 	// in the h1
 	var pickedColor = colors[getRandom(0, colors.length - 1)];	
-	document.getElementById("targetColor").textContent = pickedColor.toUpperCase();
+	document.getElementById("targetColor").textContent = pickedColor;
 	// fill squares in with randomly selected colors from the color
-	// array, then activate event listener and handle click event
-	var squares = document.querySelectorAll(".square");
-	// if(easyMode){
-	// 	for(var i = 3; i < 6; i++){
-	// 		squares[i].style.backgroundColor = "#232323";
-	// 	}
-	// }	
+	// array, then activate event listener and handle click event	
 	for(var i = 0; i < colors.length; i++) {
 		// add initial colors to squares
 		squares[i].style.backgroundColor = colors[i];
@@ -76,10 +60,10 @@ function boardSetUp() {
 			var clickedColor = this.style.backgroundColor;
 			// compare color to pickedColor
 			if(clickedColor === pickedColor){				
-				success(pickedColor, squares);
+				success(pickedColor, length);
 			} else {				
 				this.classList.add("not-visible");
-				document.getElementById("message").textContent = "Try Again";
+				message.textContent = "Try Again";
 			}
 		})
 	}
@@ -92,26 +76,37 @@ function getRandom(min, max) {
 	return Math.floor(Math.random() * max - min + 1) + min;
 }
 
-// fill color array with randomly selected colors
+// fill color array with randomly selected colors taking length as an argument
+// length is set in boardSetUp based on difficulty setting
 function colorSelector(length) {
 	var colors = [];
 	var r = 0,
 		g = 0,
 		b = 0;
+	if(length === 3) {
+		for(var i = 3; i < 6; i++) {
+			squares[i].classList.add("not-visible");
+		}
+	} else {
+		for(var i = 3; i < 6; i++) {
+			squares[i].classList.remove("not-visible");
+		}
+	}
 	for(var i = 0; i < length; i++){
 		r = getRandom(0, 255), g = getRandom(0, 255), b = getRandom(0, 255);
 		colors[i] = "rgb(" + r + ", " + g + ", " + b + ")";		
 	}		
+
 	return colors;
 }
 
 // on success, make all squares visible and change all squares to winning color
-function success(pickedColor, squares) {
-	document.getElementById("message").textContent = "Correct!";
-	document.querySelector("h1").style.backgroundColor = pickedColor;
-	for(var i = 0; i < squares.length; i++){
+function success(pickedColor, length) {
+	message.textContent = "Correct!";
+	h1.style.backgroundColor = pickedColor;	
+	for(var i = 0; i < length; i++){
 		squares[i].classList.remove("not-visible");
 		squares[i].style.backgroundColor = pickedColor;
 	}
-	document.getElementById("reset").textContent = "PLAY AGAIN?";	
+	reset.textContent = "Play Again?";	
 }
