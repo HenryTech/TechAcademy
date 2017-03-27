bb0a37cf7e1c7956c5527b516a7a557ecb6e408d
Sean.Henry <fidelicatessen@gmail.com>
2017-03-27 12:23:40 -0700

    Start jQuery section

---
+++ UdemyHTML/exercise13_jQuery.js
@@ -0,0 +1,11 @@
+// Select all divs and give them a purple background
+$("div").css("backgroundColor", "purple");
+
+// Select all divs with class "highlight" and make them 200px wide
+$("div.highlight").css("width", "200px");
+
+// Select div w/ id "third" and give it an orange border
+$("div#third").css("border", "3px solid orange");
+
+// Select first div only and change font color to pink
+$("div:first-of-type").css("color", "pink");
